186cd088726c34bb05d49faf6901098e9b850e75
priyanshuProgrammer <priyanshu22600@gmail.com>
2025-06-12 10:52:26 +0530

    wss

--- app/utils/store.ts
+++ app/utils/store.ts
@@ -1,6 +1,6 @@
 import { atom } from "jotai";
 
-export const url = "ws://blockchain-backend-e3ma.onrender.com/";
+export const url = "wss://blockchain-backend-e3ma.onrender.com/";
 export const nodes = atom<number>();
 export const reward = atom<number>(0.0);
 export const status = atom<string>("disconnected");
